6b38228225c590fb4775602ef81599561ccd0b8a
Josh Couper <joshcouper@gmail.com>
2020-02-09 15:23:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,6 @@
-# Your Code Here
-my own map
-  returns an array with all values made negative (FAILED - 1)
-  returns an array with the original values (FAILED - 2)
-  returns an array with the original values multiplied by 2 (FAILED - 3)
-  returns an array with the original values squared (FAILED - 4)
+
+# my own map
+# returns an array with all values made negative (FAILED - 1)
+#  returns an array with the original values (FAILED - 2)
+# returns an array with the original values multiplied by 2 (FAILED - 3)
+#  returns an array with the original values squared (FAILED - 4)
