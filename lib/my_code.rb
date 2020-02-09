efed976c2d8a76e1ecba721ccbf2e4ff238091d3
Josh Couper <joshcouper@gmail.com>
2020-02-09 15:54:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,6 +28,18 @@ end
 
 
 
-def reduce(array)
+def reduce(array, sv=nil)
+
+if sv 
+num1 = sv 
+i=0 
+else
+  num1 = array[0]
+  i = 1
+end
+while i < array.length do 
+ num1 = yield(num1, array[i])
+ i += 1
+end 
 
 end
